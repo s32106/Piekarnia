cf54c304ca7a2820eba5deeaedb16717716d755a
s32106 <s32106@pjwstk.edu.pl>
2026-01-11 16:19:01 +0100

    idk

--- Assets/Input/CursorController.cs
+++ Assets/Input/CursorController.cs
@@ -6,9 +6,19 @@ using UnityEngine.InputSystem;
 public class CursorController : MonoBehaviour
 {
     private Controls controls;
+    private Camera mainCamera;
+
+    private void Awake()
+    {
+        controls = new Controls();
+        mainCamera = Camera.main;
+    }
+
 
     private void DetectObject()
     {
+        Ray ray = mainCamera.ScreenPointToRay
+            (controls.Mouse.Position.ReadValue<Vector2>());
         RaycastHit2D hits2D = Physics2D.GetRayIntersection(ray);
         if (hits2D.collider != null)
             Debug.Log("Hit 2D Collider" + hits2D.collider.tag);
