bf32eb1797f53f0126b886729c9793dd33cf0178
s32106 <s32106@pjwstk.edu.pl>
2026-01-27 15:20:58 +0100

    SKONCZONE

--- Assets/Scripts/CameraSwitch.cs
+++ Assets/Scripts/CameraSwitch.cs
@@ -43,18 +43,16 @@ public class CameraSwitch : MonoBehaviour
             MainCam.gameObject.SetActive(true);
         }
 
-        //Włączenie komiksu po zebraniu wszytskich przedmiotów
-        //if (Input.GetKeyDown(KeyCode.C) && MainCam.isActiveAndEnabled)
         if (inventory.currentCollectible == 6)
         {
             ComicCam0.gameObject.SetActive(true);
             MainCam.gameObject.SetActive(false);
+            inventory.currentCollectible -= 1;
         }
 
         if (Input.GetKeyDown(KeyCode.E) && ComicCam0.isActiveAndEnabled)
         {
             ComicCam0.gameObject.SetActive(false);
-            Destroy(ComicCam0.gameObject);
             ComicCam1.gameObject.SetActive(true);
         }
 
@@ -67,24 +65,18 @@ public class CameraSwitch : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.E) && ComicCam2.isActiveAndEnabled)
         {
             ComicCam2.gameObject.SetActive(false);
-            ComicCam1.gameObject.SetActive(false);
             ComicCam3.gameObject.SetActive(true);
         }
 
         if (Input.GetKeyDown(KeyCode.Q) && ComicCam3.isActiveAndEnabled)
         {
             ComicCam3.gameObject.SetActive(false);
-            ComicCam2.gameObject.SetActive(false);
-            ComicCam1.gameObject.SetActive(false);
             ComicCam4.gameObject.SetActive(true);
         }
 
         if (Input.GetKeyDown(KeyCode.E) && ComicCam4.isActiveAndEnabled)
         {
             ComicCam4.gameObject.SetActive(false);
-            ComicCam3.gameObject.SetActive(false);
-            ComicCam2.gameObject.SetActive(false);
-            ComicCam1.gameObject.SetActive(false);
             EndCam.gameObject.SetActive(true);
         }
 
